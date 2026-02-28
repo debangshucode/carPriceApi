864bc979cea0f30b945dd48ebb3866a5e0a9f084
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-28 18:15:00 +0530

    added user services -create ,findOne ,find all ,delete ,update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,33 @@ export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>) { }
 
     create(email: string, password: string) {
-        const user = this.repo.create({email,password});
-        
+        const user = this.repo.create({ email, password });
+
         return this.repo.save(user);
+    }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({ id }); // one rec or null
+    }
+
+    find(email: string) {
+        return this.repo.find({ where: { email } }) //array of rec
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error("user not found");
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user)
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error("user not found");
+        }
+        return this.repo.remove(user)
     }
 }
