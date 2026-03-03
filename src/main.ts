e0591738c25f65030d972876ea118f344fc95785
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-03 14:36:55 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder} from '@nestjs/swagger'
 const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const config = new DocumentBuilder()
+  .setTitle('Car-Price')
+  .setDescription('This is a car price api includes user auth and car repors for make purchase decisions ')
+  .setVersion('1.0')
+  .addTag('cars')
+  .build()
+
+  const documentFactory = ()=> SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,documentFactory);
+
+  
   app.use(cookieSession({
     keys:['asdfghnh']
   }))
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist:true, //remove extra properties autometicaly in requests 
     }),
   )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
